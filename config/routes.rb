0c4367410fa9703028e7c840ca3855b0354d00c0
EC2 Default User <ec2-user@ip-172-31-45-240.ec2.internal>
2022-01-06 12:40:12 +0000

    20220106

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
    # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get 'lists/new'
-  get '/top' => "homes#top"
+  get 'top' => "homes#top"
   post 'lists' => 'lists#create'
-  get 'lists' => 'lists#index'
+  get 'lists' => 'lists#index',as: 'lists_index'
   #.../lists/1や.../lists/3に該当する
   get 'lists/:id' => 'lists#show',as: 'list'
   get 'lists/:id/edit' => 'lists#edit',as: 'edit_list'
